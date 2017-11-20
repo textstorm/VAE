a729ced89c02440872f32f6b971bb2f370a5cb83
textstorm <jiangzhewise@gmail.com>
2017-11-20 14:23:03 +0800

    vae second version

--- vae.py
+++ vae.py
@@ -1,107 +1,106 @@
 
 import tensorflow as tf
 
-def weight_variable(shape, name):
+def weight_variable(shape, name, initializer=None):
   initializer = tf.truncated_normal_initializer(stddev=0.1)
+  if initializer:
+    initializer = initializer
   return tf.get_variable(shape=shape, initializer=initializer, name=name)
 
-def bias_variable(shape, name):
+def bias_variable(shape, name, initializer=None):
   initializer = tf.constant_initializer(0.)
+  if initializer:
+    initializer = initializer
   return tf.get_variable(shape=shape, initializer=initializer, name=name)
 
 class VAE(object):
-  def __init__(self, args, session):
-    self._input_dim = args.input_dim
-    self._hidden_dim = args.hidden_dim
-    self._latent_dim = args.latent_dim
-    self._sess = session
-    self._max_grad_norm = args.max_grad_norm
+  def __init__(self, args, sess, name="vae"):
+    self.input_dim = args.input_dim
+    self.hidden_dim = args.hidden_dim
+    self.latent_dim = args.latent_dim
+    self.sess = sess
+    self.max_grad_norm = args.max_grad_norm
+    self.learning_rate = args.learning_rate
 
     self._build_placeholder()
     
-    self._optimizer = tf.train.AdamOptimizer(self.lr)
-    self.global_step = tf.get_variable('global_step', [], 'int32', tf.constant_initializer(0), trainable=False)
-
-    self._build_forward()
+    self.optimizer = tf.train.AdamOptimizer(self.learning_rate)
+    self.global_step = tf.Variable(0, trainable=False)
+    self._build_graph()
     self._build_loss()
     self._build_train()
     
     self.summary = tf.summary.merge_all()
     init_op = tf.global_variables_initializer()
-    self._sess.run(init_op)
+    self.sess.run(init_op)
 
   def _build_placeholder(self):
-    with tf.name_scope("Data"):
-      self.x_images = tf.placeholder(tf.float32, [None, self._input_dim])
-      self.z = tf.placeholder(tf.float32, [None, self._latent_dim])
-    with tf.name_scope("Hyparameter"):
-      self.lr = tf.placeholder(tf.float32, [], name="learning_rate")
+    with tf.name_scope("data"):
+      self.x_images = tf.placeholder(tf.float32, [None, self.input_dim])
+      self.z = tf.placeholder(tf.float32, [None, self.latent_dim])
 
-  def _build_forward(self):
-    with tf.name_scope("Model"):
-      with tf.variable_scope("Encoder"):
-        self.mu, self.log_sigma_sq = self.encoder(self.x_images)
-      with tf.variable_scope("Decoder"):
+  def _build_graph(self):
+    with tf.name_scope("model"):
+      with tf.variable_scope("encoder"):
+        self.mu, self.log_sigma_sq = self.build_encoder(self.x_images)
+      with tf.variable_scope("decoder"):
         sample_z = self.sample_z(self.mu, self.log_sigma_sq)
-        self.logits, self.reconstruct = self.decoder(sample_z)
+        self.logits, self.reconstruct = self.build_decoder(sample_z)
 
   def _build_loss(self):
-    with tf.name_scope("Loss"):
-      rec_loss = tf.reduce_sum(tf.nn.sigmoid_cross_entropy_with_logits(logits=self.logits, labels=self.x_images), 1)
-      kl_loss = -0.5 * tf.reduce_sum(1. + self.log_sigma_sq - self.mu ** 2 - tf.exp(self.log_sigma_sq), 1)
+    with tf.name_scope("loss"):
+      rec_loss = tf.reduce_sum(tf.nn.sigmoid_cross_entropy_with_logits(
+          logits=self.logits, labels=self.x_images), 1)
+      kl_loss = -0.5 * tf.reduce_sum(
+          1. + self.log_sigma_sq - self.mu ** 2 - tf.exp(self.log_sigma_sq), 1)
       self.loss_op = tf.reduce_mean(rec_loss + kl_loss)
 
   def _build_train(self):
-    with tf.name_scope('Train'):
-      grads_and_vars = self._optimizer.compute_gradients(self.loss_op)
-      grads_and_vars = [(tf.clip_by_norm(g, self._max_grad_norm), v) for g, v in grads_and_vars]
-      self.train_op = self._optimizer.apply_gradients(grads_and_vars, global_step=self.global_step)
+    with tf.name_scope('train'):
+      grads_and_vars = self.optimizer.compute_gradients(self.loss_op)
+      grads_and_vars = [(tf.clip_by_norm(g, self.max_grad_norm), v) for g, v in grads_and_vars]
+      self.train_op = self.optimizer.apply_gradients(grads_and_vars, global_step=self.global_step)
 
-  def batch_fit(self, x_images, lr, get_summary=False):
-    feed_dict = {}
-    feed_dict[self.x_images] = x_images
-    feed_dict[self.lr] = lr
-    loss, _, = self._sess.run([self.loss_op, self.train_op], feed_dict=feed_dict)
+  def train(self, x_images, get_summary=False):
+    feed_dict = {self.x_images: x_images}
+    loss, _, = self.sess.run([self.loss_op, self.train_op], feed_dict=feed_dict)
 
     return loss
 
-  def encoder(self, x_images):
-    e_w1 = weight_variable([self._input_dim, self._hidden_dim], name="E_W1")
-    e_b1 = bias_variable([self._hidden_dim], name="E_b1")
-    e_h1 = tf.nn.relu(tf.matmul(x_images, e_w1) + e_b1)
-    e_wmu = weight_variable([self._hidden_dim, self._latent_dim], name="E_Wmu")
-    e_bmu = bias_variable([self._latent_dim], name="E_bmu")
-    e_hmu = tf.matmul(e_h1, e_wmu) + e_bmu
-    e_wlog_sigma_sq = weight_variable([self._hidden_dim, self._latent_dim], name="E_Wlog_sigma_sq")
-    e_blog_sigma_sq = bias_variable([self._latent_dim], name="E_blog_sigma_sq")
-    e_hlog_sigma_sq = tf.matmul(e_h1, e_wlog_sigma_sq) + e_blog_sigma_sq
+  def build_encoder(self, x_images):
+    with tf.variable_scope("encoder"):
+      x = x_images
+      x = tf.layers.dense(x, self.hidden_dim, name='en_layer1')
+      x = tf.nn.relu(x)
+      x = tf.layers.dense(x, self.hidden_dim, name='en_layer2')
+      x = tf.nn.relu(x)
+      x = tf.layers.dense(x, self.hidden_dim, name='en_layer3')
+      x = tf.nn.relu(x)
+      z_mu = tf.layers.dense(x, self.latent_dim, name='layer_mu')
+      z_log_sigma_sq = tf.layers.dense(x, self.latent_dim, name='layer_log_sigma_sq')
+      return z_mu, z_log_sigma_sq
 
-    return e_hmu, e_hlog_sigma_sq
+  def build_decoder(self, z):
+    with tf.variable_scope("decoder"):
+      x = z
+      x = tf.layers.dense(x, self.hidden_dim, name='de_layer1')
+      x = tf.nn.relu(x)
+      x = tf.layers.dense(x, self.hidden_dim, name='de_layer2')
+      x = tf.nn.relu(x)
+      x = tf.layers.dense(x, self.input_dim, name='de_layer3')
+      x_recons = tf.nn.sigmoid(x)
+      return x, x_recons
 
   def sample_z(self, mu, log_sigma_sq):
     eps = tf.random_normal(shape=tf.shape(mu))
-
     return mu + tf.exp(log_sigma_sq / 2) * eps
-
-  def decoder(self, z):
-    d_w1 = weight_variable([self._latent_dim, self._hidden_dim], name="D_W1")
-    d_b1 = bias_variable([self._hidden_dim], name="D_b1")
-    d_h1 = tf.nn.relu(tf.matmul(z, d_w1) + d_b1)
-    d_w2 = weight_variable([self._hidden_dim, self._input_dim], name="D_W2")
-    d_b2 = bias_variable([self._input_dim], name="D_b2")
-    d_l2 = tf.matmul(d_h1, d_w2) + d_b2
-    d_h2 = tf.nn.sigmoid(d_l2)
-
-    return d_l2, d_h2
 
   def generate(self, z):
     feed_dict= {self.z: z}
-    with tf.variable_scope("Decoder", reuse=True):
-      _, d_h2 = self.decoder(self.z)
-    
-    return self._sess.run(d_h2, feed_dict=feed_dict)
+    with tf.variable_scope("decoder", reuse=True):
+      logits, x_recons = self.build_decoder(self.z)
+    return self.sess.run(x_recons, feed_dict=feed_dict)
 
   def reconstruct(self, x_images):
     feed_dict = {self.x_images: x_images}
-    
-    return self._sess.run(self.reconstruct, feed_dict)
+    return self.sess.run(self.reconstruct, feed_dict)
