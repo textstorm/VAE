fa362c84b967b70ace3b82b951e23d7ebe3a6e89
textstorm <jiangzhewise@gmail.com>
2017-11-22 19:52:52 +0800

    config second version

--- conv VAE/config.py
+++ conv VAE/config.py
@@ -12,17 +12,17 @@ def get_args():
   parser.add_argument('--train_dir', type=str, default='/root/textstorm/VAE/vanilla VAE/data/')
   parser.add_argument('--log_dir', type=str, default='/root/textstorm/VAE/conv VAE/logs/')
   parser.add_argument('--save_dir', type=str, default='/root/textstorm/VAE/conv VAE/saves/')
-  parser.add_argument('--nb_classes', type=int, default=10)
+  parser.add_argument('--nb_classes', type=int, default=10, help='nb_classes')
 
   parser.add_argument('--input_dim', type=int, default=784, help='Dimension of input data')
   parser.add_argument('--hidden_dim', type=int, default=256, help="Dimension of hidden layer")
-  parser.add_argument('--latent_dim', type=int, default=64, help="Dimension of latent variable")
+  parser.add_argument('--latent_dim', type=int, default=100, help="Dimension of latent variable")
 
-  parser.add_argument('--batch_size', type=int, default=64, help='Train batch size')
+  parser.add_argument('--batch_size', type=int, default=100, help='Train batch size')
   parser.add_argument('--nb_epoch', type=int, default=1000, help='The number of epoch')
-  parser.add_argument('--learning_rate', type=float, default=0.001, help='Learning rate')
+  parser.add_argument('--learning_rate', type=float, default=0.0001, help='Learning rate')
   parser.add_argument('--max_grad_norm', type=float, default=10.0, help='Max norm of gradient')
   parser.add_argument('--log_period', type=int, default=100, help='number step to print')
-  parser.add_argument('--save_period', type=int, default=100, help='number epoch to save')
+  parser.add_argument('--save_period', type=int, default=50, help='number epoch to save')
 
   return parser.parse_args()
