cf937831fcd28055b59c2e18a4636a4223e1adea
textstorm <jiangzhewise@gmail.com>
2018-01-01 11:58:58 +0800

    debug

--- model.py
+++ model.py
@@ -18,24 +18,11 @@ class Base(object):
       self.x_images = tf.placeholder(tf.float32, [None, self.input_dim])
       self.z = tf.placeholder(tf.float32, [None, self.latent_dim])
 
+    self.batch_size = tf.shape(self.x_images)[0]
+
   def sample_z(self, mu, log_sigma_sq):
     eps = tf.random_normal(shape=tf.shape(mu))
     return mu + tf.exp(log_sigma_sq / 2) * eps
-
-  def build_encoder(self, x_images):
-    x = tf.layers.dense(x_images, self.hidden_dim, activation=tf.nn.relu, name='en_layer1')
-    x = tf.layers.dense(x, self.hidden_dim, activation=tf.nn.relu, name='en_layer2')
-    x = tf.layers.dense(x, self.hidden_dim, activation=tf.nn.relu, name='en_layer3')
-    z_mu = tf.layers.dense(x, self.latent_dim, name='layer_mu')
-    z_log_sigma_sq = tf.layers.dense(x, self.latent_dim, name='layer_log_sigma_sq')
-    return z_mu, z_log_sigma_sq
-
-  def build_decoder(self, z):
-    x = tf.layers.dense(z, self.hidden_dim, activation=tf.nn.relu, name='de_layer1')
-    x = tf.layers.dense(x, self.hidden_dim, activation=tf.nn.relu, name='de_layer2')
-    x = tf.layers.dense(x, self.input_dim, activation=None, name='de_layer3')
-    x_recons = tf.nn.sigmoid(x)
-    return x, x_recons
 
   def train(self, x_images):
     feed_dict = {self.x_images: x_images}
@@ -100,6 +87,21 @@ class VAE(Base):
     self.summary = tf.summary.merge_all()
     self.saver = tf.train.Saver(tf.global_variables())
 
+  def build_encoder(self, x_images):
+    x = tf.layers.dense(x_images, self.hidden_dim, activation=tf.nn.relu, name='en_layer1')
+    x = tf.layers.dense(x, self.hidden_dim, activation=tf.nn.relu, name='en_layer2')
+    x = tf.layers.dense(x, self.hidden_dim, activation=tf.nn.relu, name='en_layer3')
+    z_mu = tf.layers.dense(x, self.latent_dim, name='layer_mu')
+    z_log_sigma_sq = tf.layers.dense(x, self.latent_dim, name='layer_log_sigma_sq')
+    return z_mu, z_log_sigma_sq
+
+  def build_decoder(self, z):
+    x = tf.layers.dense(z, self.hidden_dim, activation=tf.nn.relu, name='de_layer1')
+    x = tf.layers.dense(x, self.hidden_dim, activation=tf.nn.relu, name='de_layer2')
+    x = tf.layers.dense(x, self.input_dim, activation=None, name='de_layer3')
+    x_recons = tf.nn.sigmoid(x)
+    return x, x_recons
+
 class DCVAE(Base):
   def __init__(self, args, sess, name="dcvae"):
     super(DCVAE, self).__init__(args=args, sess=sess, name=name)
@@ -134,9 +136,9 @@ class DCVAE(Base):
 
   def build_encoder(self, x_images):
     x = tf.reshape(x_images, [-1, 28, 28, 1])
-    x = conv2d(x, 3, 1, 10, 2, 'en_layer1')
-    x = conv2d(x, 3, 10, 10, 2, 'en_layer2')
-    x = conv2d(x, 3, 10, 10, 2, 'en_layer3')
+    x = self.conv2d(x, 3, 1, 10, 2, 'en_layer1')
+    x = self.conv2d(x, 3, 10, 10, 2, 'en_layer2')
+    x = self.conv2d(x, 3, 10, 10, 2, 'en_layer3')
     b, h, w, c = x.get_shape().as_list()
     x = tf.reshape(x, [-1, h * w * c])
     z_mu = tf.layers.dense(x, self.latent_dim, name='layer_mu')
@@ -146,9 +148,67 @@ class DCVAE(Base):
   def build_decoder(self, z):
     x = tf.layers.dense(z, 160, activation=tf.nn.relu, name='de_layer1')
     x = tf.reshape(x, [-1, 4, 4, 10])
-    x = deconv2d(x, 3, 10, 10, 2, [-1, 7, 7, 10], name='de_layer2')
-    x = deconv2d(x, 3, 10, 10, 2, [-1, 14, 14, 10], name='de_layer3')
-    x = deconv2d(x, 3, 1, 10, 2, [-1, 28, 28, 1], False, name='de_layer4')
+    x = self.deconv2d(x, 3, 10, 10, 2, [self.batch_size, 7, 7, 10], name='de_layer2')
+    x = self.deconv2d(x, 3, 10, 10, 2, [self.batch_size, 14, 14, 10], name='de_layer3')
+    x = self.deconv2d(x, 3, 1, 10, 2, [self.batch_size, 28, 28, 1], False, name='de_layer4')
     x = tf.reshape(x, [-1, self.input_dim])
     x_reconstruct = tf.nn.sigmoid(x)
     return x, x_reconstruct
+
+  def conv2d(self, x, filter_size, n_in, n_out, strides, name=None):
+    w = self.weight_variable(shape=[filter_size, filter_size, n_in, n_out], name=name+'_w')
+    b = self.bias_variable(shape=[n_out], name=name+'_b')
+    x = tf.nn.conv2d(input=x, 
+                     filter=w, 
+                     strides=[1, strides, strides, 1],
+                     padding='SAME',
+                     name=name)
+    x = tf.nn.bias_add(x, b)
+    x = tf.nn.relu(x)
+    return x
+
+  def deconv2d(self, x, filter_size, n_in, n_out, strides, output_shape, use_activation=True, name=None):
+    w = self.weight_variable(shape=[filter_size, filter_size, n_in, n_out], name=name+'_w')
+    b = self.bias_variable(shape=[n_in], name=name+'_b')
+    output_shape = tf.stack(output_shape)
+    x = tf.nn.conv2d_transpose(value=x,
+                               filter=w,
+                               output_shape=output_shape,
+                               strides=[1, strides, strides, 1],
+                               padding='SAME',
+                               name=name)
+    x = tf.nn.bias_add(x, b)
+    if use_activation:
+      x = tf.nn.relu(x)
+    return x
+
+  def generate(self, z, batch_size):
+    feed_dict= {self.z: z, self.batch_size: batch_size}
+    with tf.variable_scope("decoder", reuse=True):
+      logits, x_recons = self.build_decoder(self.z)
+    return self.sess.run(x_recons, feed_dict=feed_dict)
+
+class LVAE(Base):
+  def __init__(self, args, sess, name="lvae"):
+    super(LVAE, self).__init__(args=args, sess=sess, name=name)
+    self.hidden_sizes = args.hidden_sizes
+    self.latent_sizes = args.latent_sizes
+    self.latent_length = len(self.latent_sizes)
+    self.encoder_mus, self.encoder_logvars = [0] * self.latent_length, [0] * self.latent_length
+    self.decoder_mus, self.decoder_logvars = [0] * self.latent_length, [0] * self.latent_length
+    self.prior_mus, self.prior_logvars = [0] * self.latent_length, [0] * self.latent_length
+
+  def build_encoder(self, x_images):
+    h = x_images
+    for idx, hsize in enumerate(self.hidden_sizes):
+      h = tf.layers.dense(h, hsize)
+      h = tf.layers.batch_normalization(h)
+      h = tf.nn.relu(h)
+      self.encoder_mus[idx], self.encoder_logvars[idx] = self.build_latent(h)
+
+  def build_decoder(self, z):
+    
+  def build_latent(self, hidden):
+    z_mu = tf.layers.dense(x, self.latent_dim, name='layer_mu')
+    z_log_sigma_sq = tf.layers.dense(x, self.latent_dim, name='layer_log_sigma_sq')
+    return z_mu, z_log_sigma_sq
